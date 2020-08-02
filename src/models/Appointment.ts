6141adf1aa28b1c93268034c5a8121388855c64f
Giovanna <giovannabadaroalves@gmail.com>
2020-08-01 22:54:26 -0300

    migration alter user foreign key

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -3,8 +3,12 @@ import {
   Column,
   PrimaryGeneratedColumn, 
   UpdateDateColumn, 
-  CreateDateColumn 
+  CreateDateColumn, 
+  ManyToOne,
+  JoinColumn
 } from 'typeorm';
+
+import User from './User'
 
 @Entity('appointments') 
 class Appointment {
@@ -12,7 +16,11 @@ class Appointment {
   id: string;
 
   @Column()
-  provider: string;
+  provider_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({name: 'provider_id' })
+  provider: User;
 
   @Column('time with time zone')
   date: Date;
