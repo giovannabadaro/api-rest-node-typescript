0cd72d813ca07b9c9895db0e54bd2937776613f2
Giovanna <giovannabadaroalves@gmail.com>
2020-08-01 22:23:05 -0300

    migration users

--- src/database/migrations/1596330375614-CreateUsers.ts
+++ src/database/migrations/1596330375614-CreateUsers.ts
@@ -1,28 +1,33 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
-export default class CreateAppointments1596317187995 implements MigrationInterface {
+export default class CreateUsers1596330375614 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name: 'appointments',
+                name: 'users',
                 columns: [
                     {
                         name: 'id',
                         type: 'varchar',
                         isPrimary: true,
-                        generationStrategy : 'uuid',
+                        generationStrategy: 'uuid',
                         default: 'uuid_generate_v4()'
                     },
                     {
-                        name: 'provider',
+                        name: 'name',
                         type: 'varchar',
                         isNullable: false,
                     },
                     {
-                        name: 'date',
-                        type: 'timestamp with time zone',
+                        name: 'email',
+                        type: 'varchar',
+                        isUnique: true,
                         isNullable: false,
+                    },
+                    {
+                        name: 'password',
+                        type: 'varchar'               
                     },
                     {
                         name: 'created_at',
@@ -40,7 +45,7 @@ export default class CreateAppointments1596317187995 implements MigrationInterfa
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('appointments')
+        await queryRunner.dropTable('users')
     }
 
 }
